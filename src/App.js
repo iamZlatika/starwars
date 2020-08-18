55b828dbbda2116e0c74e14632ef085191a4ad9f
iamZlatika <mazurovazv@gmail.com>
2020-08-18 17:26:52 +0300

    install React Query Dev Tools

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import People from './components/People';
 import Planets from './components/Planets';
+import { ReactQueryDevtools } from 'react-query-devtools';
 
 function App() {
-const [page, setPage] = useState('planets')
+  const [page, setPage] = useState('planets')
 
   return (
+    <>
     <div className="App">
-     <h1>StarWar Info</h1>
-     <Navbar 
-     setPage = {setPage}/>
-     <div className="content">
-    {page === 'planets' ? <Planets /> : <People />}
-     </div>
+      <h1>StarWar Info</h1>
+      <Navbar
+        setPage={setPage} />
+      <div className="content">
+        {page === 'planets' ? <Planets /> : <People />}
+      </div>
     </div>
+    
+     <ReactQueryDevtools initialsIsOpen={false} />
+    </> 
   );
 }
 
